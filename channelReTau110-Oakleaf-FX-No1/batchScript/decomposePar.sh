772745a42b21d79932d78ff59b24a911bc7d6a7e
masazzz <masashi.imano@gmail.com>
2015-05-15 14:28:57 +0900

    Add channelReTau110-Oakleaf-FX-No1

---
+++ channelReTau110-Oakleaf-FX-No1/batchScript/decomposePar.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#PJM -L "rscgrp=lecture"
+#PJM -L "node=1"
+#PJM -j
+#PJM -S
+#PJM -g "gt00"
+
+module load OpenFOAM/2.3.0
+unset FOAM_INST_DIR
+export USER=$PJM_O_LOGNAME
+source $WM_PROJECT_DIR/etc/bashrc
+
+# Source tutorial run functions
+. $WM_PROJECT_DIR/bin/tools/RunFunctions
+
+env
+
+decomposePar -cellDist >& log.decomposePar
+
---
+++ channelReTau110-Oakleaf-FX-No1/log.benchmark.sh
@@ -0,0 +1,33 @@
+loop= 1
+n= 00001
+[INFO] PJM 0000 pjsub Job 1871349 submitted.
+mpi= 00192
+[INFO] PJM 0000 pjsub Job 1871350 submitted.
+dir= n_00001/mpi_00192/simulationType_laminar-LESModel_laminar-delta_cubeRootVol-solver_PCG-preconditioner_FDIC
+[INFO] PJM 0000 pjsub Job 1871352 submitted.
+dir= n_00001/mpi_00192/simulationType_laminar-LESModel_laminar-delta_cubeRootVol-solver_GAMG-smoother_FDIC
+[INFO] PJM 0000 pjsub Job 1871353 submitted.
+mpi= 00128
+[INFO] PJM 0000 pjsub Job 1871354 submitted.
+dir= n_00001/mpi_00128/simulationType_laminar-LESModel_laminar-delta_cubeRootVol-solver_PCG-preconditioner_FDIC
+[INFO] PJM 0000 pjsub Job 1871355 submitted.
+dir= n_00001/mpi_00128/simulationType_laminar-LESModel_laminar-delta_cubeRootVol-solver_GAMG-smoother_FDIC
+[INFO] PJM 0000 pjsub Job 1871356 submitted.
+mpi= 00064
+[INFO] PJM 0000 pjsub Job 1871357 submitted.
+dir= n_00001/mpi_00064/simulationType_laminar-LESModel_laminar-delta_cubeRootVol-solver_PCG-preconditioner_FDIC
+[INFO] PJM 0000 pjsub Job 1871358 submitted.
+dir= n_00001/mpi_00064/simulationType_laminar-LESModel_laminar-delta_cubeRootVol-solver_GAMG-smoother_FDIC
+[INFO] PJM 0000 pjsub Job 1871359 submitted.
+mpi= 00032
+[INFO] PJM 0000 pjsub Job 1871360 submitted.
+dir= n_00001/mpi_00032/simulationType_laminar-LESModel_laminar-delta_cubeRootVol-solver_PCG-preconditioner_FDIC
+[INFO] PJM 0000 pjsub Job 1871361 submitted.
+dir= n_00001/mpi_00032/simulationType_laminar-LESModel_laminar-delta_cubeRootVol-solver_GAMG-smoother_FDIC
+[INFO] PJM 0000 pjsub Job 1871362 submitted.
+mpi= 00016
+[INFO] PJM 0000 pjsub Job 1871363 submitted.
+dir= n_00001/mpi_00016/simulationType_laminar-LESModel_laminar-delta_cubeRootVol-solver_PCG-preconditioner_FDIC
+[INFO] PJM 0000 pjsub Job 1871364 submitted.
+dir= n_00001/mpi_00016/simulationType_laminar-LESModel_laminar-delta_cubeRootVol-solver_GAMG-smoother_FDIC
+[INFO] PJM 0000 pjsub Job 1871365 submitted.
