308e9d42c2bea72e316cf560eed3ae307539df1c
masazzz <masashi.imano@gmail.com>
2015-05-28 21:05:16 +0900

    Change for FOCUS D system

--- channelReTau110/FOCUS/batchScript/blockMesh.sh
+++ channelReTau110/FOCUS/batchScript/blockMesh.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
-#SBATCH -p debug6m
-#SBATCH -n 1
-#SBATCH -J OFGcc
-#SBATCH -e OFGcc.e%J
-#SBATCH -o OFGcc.o%J
-
-# Path
-OpenFOAM_PATH=/home1/share/openfoam/2.3.0/gnu/openmpi/OpenFOAM-2.3.0
-VTUNE_PATH=/home1/share/opt/intel/vtune_amplifier_xe/bin64
-MPI_PATH=/home1/share/openmpi/1.6.5/gnu/bin
 
 module load gnu/openmpi165
 unset FOAM_INST_DIR
-source $OpenFOAM_PATH/etc/bashrc
-
-# Source tutorial run functions
-. $WM_PROJECT_DIR/bin/tools/RunFunctions
+source /home1/share/openfoam/2.3.0/gnu/openmpi/OpenFOAM-2.3.0/etc/bashrc
 
 env
 
--- channelReTau110/FOCUS/batchScript/solve.sh
+++ channelReTau110/FOCUS/batchScript/solve.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-#SBATCH -p debug6m
-#SBATCH -J OFGcc
-#SBATCH -e OFGcc.e%J
-#SBATCH -o OFGcc.o%J
 
 # Path
-OpenFOAM_PATH=/home1/share/openfoam/2.3.0/gnu/openmpi/OpenFOAM-2.3.0
 VTUNE_PATH=/home1/share/opt/intel/vtune_amplifier_xe/bin64
 MPI_PATH=/home1/share/openmpi/1.6.5/gnu/bin
 
@@ -14,10 +9,7 @@ export INTEL_LICENSE_FILE=/home1/share/opt/intel/flexlm
 
 module load gnu/openmpi165
 unset FOAM_INST_DIR
-source $OpenFOAM_PATH/etc/bashrc
-
-# Source tutorial run functions
-. $WM_PROJECT_DIR/bin/tools/RunFunctions
+source /home1/share/openfoam/2.3.0/gnu/openmpi/OpenFOAM-2.3.0/etc/bashrc
 
 env > log.env
 
