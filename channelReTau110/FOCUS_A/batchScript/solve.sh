f72880964b31a593ce7aad4bab4d8f1cf9e04082
masazzz <masashi.imano@gmail.com>
2015-05-29 09:46:00 +0900

    Add example for FOCUS A,C,D,G systems

--- channelReTau110/FOCUS_A/batchScript/solve.sh
+++ channelReTau110/FOCUS_A/batchScript/solve.sh
@@ -11,6 +11,9 @@ module load gnu/openmpi165
 unset FOAM_INST_DIR
 source /home1/share/openfoam/2.3.0/gnu/openmpi/OpenFOAM-2.3.0/etc/bashrc
 
+# Source tutorial run functions
+. $WM_PROJECT_DIR/bin/tools/RunFunctions
+
 env > log.env
 
 log=log.$SLURM_JOB_ID
