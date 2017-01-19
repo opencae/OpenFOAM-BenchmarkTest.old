e23c82c659b1cc83ed6e7a1cb465a03e2e2fff2b
masazzz <masashi.imano@gmail.com>
2017-01-19 11:42:46 +0900

    Add Oakforest-PACS-mesh_3M-No1

---
+++ channelReTau110/Oakforest-PACS-mesh_3M-No1/share/batchScript/decomposePar.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if [ -z "${PJM_JOBID+x}" ] ; then
+    PJM_JOBID=$1
+    mpi=1
+else
+    export HOME=/work/$(id -gn)/$(id -un)
+    cd $PJM_O_WORKDIR
+    mpi=${PJM_MPI_PROC}
+fi
+
+# OpenFOAM-v1612+, ThirdParty Gcc, INTELMPI 2017.1.132
+module purge
+module load gcc/4.8.5
+module unload impi
+module load impi/2017.1.132
+export MPI_ROOT=$I_MPI_ROOT
+export I_MPI_DEBUG=5
+. ${HOME}/OpenFOAM/OpenFOAM-v1612+/etc/bashrc \
+    WM_COMPILER_TYPE=ThirdParty \
+    WM_COMPILER=Gcc \
+    WM_MPLIB=INTELMPI \
+    WM_LABEL_SIZE=32
+
+# Application name
+application=decomposePar.sh
+
+# Log file
+log=log.${application}.${PJM_JOBID}
+batchFileDone=${application}.done
+
+env > $log 2>&1 
+if [ $mpi -eq 1 ];then
+    decomposePar >> $log 2>&1
+else
+    mpirun redistributePar -decompose -parallel >> $log 2>&1 
+fi
+grep "^End" $log >& /dev/null && touch $batchFileDone
+
+#------------------------------------------------------------------------------
