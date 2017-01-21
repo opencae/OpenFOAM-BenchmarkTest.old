ae3c0a5436bd3c470da253dcc24ea23e9ee11c9b
Masashi Imano <masashi.imano@gmail.com>
2017-01-21 17:44:45 +0900

    Add another result of Oakforest-PACS-mesh_24M-No2

--- channelReTau110/Oakforest-PACS-mesh_24M-No2/plot.sh
+++ channelReTau110/Oakforest-PACS-mesh_24M-No2/plot.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-batchFileName="\
-v1612+_ThirdParty_GccKNL_INTELMPI_64_cache_0 \
-v1612+_ThirdParty_Gcc_INTELMPI_64_cache_0 \
-v1612+_ThirdParty_IccKNL_INTELMPI_64_cache_0 \
-v1612+_ThirdParty_Icc_INTELMPI_64_cache_0 \
+for ppn in 64 128 256
+do
+    batchFileName="\
+v1612+_system_Icc_INTELMPI_${ppn}_flat_3 \
+v1612+_system_IccKNL_INTELMPI_${ppn}_flat_3 \
+v1612+_system_Icc_INTELMPI_${ppn}_cache_0 \
+v1612+_system_IccKNL_INTELMPI_${ppn}_cache_0 \
 "
-
-../bin/plot.py all.csv -b $batchFileName
-../bin/plot.py all.csv -y -b $batchFileName
+    ../bin/plot.py all.csv -b $batchFileName
+    ../bin/plot.py all.csv -y -b $batchFileName
+    mkdir ppn-${ppn}
+    mv *.pdf ppn-${ppn}
+done
